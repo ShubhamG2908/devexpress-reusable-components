d36bf6f620d1f0042dc730579bef55d45e0a4550
tushard_itpath <tushard@itpathsolutions.com>
2024-11-29 13:58:17 +0530

    Managed Data grid with dynamic approach and reusable n level properties and methods. Optimize some code

---
+++ MVCDemoApp/Controllers/ComponentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemoApp.Controllers
+{
+	public class ComponentsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult ButtonComponents()
+		{
+			return PartialView("Button/_ButtonComponents.cshtml");
+		}
+	}
+}
